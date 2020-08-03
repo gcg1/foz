baaabba377b93c00dd904ba33a76241963a4bcf0
George <georgefbcunliffe@gmail.com>
2020-08-03 13:06:46 +0100

    Updates

---
+++ component---src-pages-components-footer-js-e5eb1c5809a3797ba781.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{qFMz:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),l=n.n(t);a.default=function(){return l.a.createElement("footer",null,l.a.createElement("div",{className:"grid-parent"},l.a.createElement("div",{className:"nav grid-item"},l.a.createElement("ul",null,l.a.createElement("li",null,l.a.createElement("a",{href:"mailto:info@foz.agency"},"info@foz.agency")),l.a.createElement("li",null,"10 York Road, London, SE1 7ND")))))}}}]);
+//# sourceMappingURL=component---src-pages-components-footer-js-e5eb1c5809a3797ba781.js.map
