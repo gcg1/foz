ccafa324c38f4c621889128a2e320add65b6b013
George <georgefbcunliffe@gmail.com>
2020-08-10 16:37:29 +0100

    Adjusted footer layout

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
       <div className="grid-parent">
         <div className="nav grid-item">
           <ul>
-            {/* <li>
-              <a href="mailto:info@foz.agency">info@foz.agency</a>
-            </li> */}
+            <li>
+              {/* <a href="mailto:info@foz.agency">info@foz.agency</a> */}
+            </li>
             <li>10 York Road, London, SE1 7ND</li>
           </ul>
         </div>
